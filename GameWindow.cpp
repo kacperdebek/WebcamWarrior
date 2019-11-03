3ffff36416a7db83b01db6aa122dcdc74315177a
pk-36 <pk-36@sr.dmcs.pl>
2019-11-03 21:38:55 +0100

    Dodanie pierwszej wersji menu

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -1,5 +1,7 @@
 #include "WebcamControl.hpp"
-#define SPAWN_DELAY 10
+#include <iostream>
+#include "Menu.hpp"
+#define SPAWN_DELAY 3
 void initializeText(sf::Text& text, sf::Font& font, int textSize, int xPosition, int yPosition, const String& label, const sf::Color& color)
 {
 	text.setFont(font);
@@ -43,22 +45,27 @@ int main()
 	initializeCircle(aim, 10, sf::Color::Blue);
     target.setOutlineColor(sf::Color::Red);
     target.setOutlineThickness(5);
-    sf::Clock deltaClock;
+    
     sf::Time dt;
     bool spacePressed = false;
     bool targetShot = false;
+	bool playPressed = false;
     int randX = rand() % 540 + 50;
     int randY = rand() % 360 + 50;
     target.setPosition(randX, randY);
+	Menu menu(640, 480);
+	sf::Clock deltaClock;
+	static std::once_flag onceFlag;
     while (window.isOpen())
     {
+		
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
             if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Space) {
+                if (event.key.code == sf::Keyboard::Space && playPressed) {
                     if ((abs(randX - webcamThread.getX()) < 30) && (abs(randY - webcamThread.getY()) < 30) && !spacePressed) {
                         spacePressed = true;
                         if (!targetShot) {
@@ -70,7 +77,7 @@ int main()
                             dt = sf::seconds(9.5);
                         }
                     }
-                    else if (!spacePressed)
+                    else if (!spacePressed && playPressed)
                     {
                         spacePressed = true;
                         cout << "Miss!" << endl;
@@ -79,35 +86,68 @@ int main()
                 }
             }
             if (event.type == sf::Event::KeyReleased) {
-                if (event.key.code == sf::Keyboard::Space) {
+                if (event.key.code == sf::Keyboard::Space && playPressed) {
                     spacePressed = false;
                 }
+				else if (event.key.code == sf::Keyboard::Up)
+					menu.MoveUp();
+				else if (event.key.code == sf::Keyboard::Down)
+					menu.MoveDown();
+				else if (event.key.code == sf::Keyboard::Enter)
+				{
+					switch (menu.GetPressedItem())
+					{
+						case 0:
+							std::cout << "Play button has been pressed" << std::endl;
+							playPressed = true;
+							break;
+						case 1:
+							std::cout << "Option button has been pressed" << std::endl;
+							break;
+						case 2:
+							thread.terminate();
+							window.close();
+							break;
+					}
+				}
             }
         }
-        aim.setPosition(webcamThread.getX(), webcamThread.getY());
-        if (dt <= sf::seconds(SPAWN_DELAY))
-        {
-            window.clear();
-            pointTotal.setString("Points: " + to_string(points));
-            window.draw(pointTotal);
-            window.draw(target);
-            if (webcamThread.getX() < 0 || webcamThread.getY() < 0) {
-                window.draw(gunpointNotFound);
-            }
-            else {
-                window.draw(aim);
-            }
-        }
-        else
-        {
-            targetShot = false;
-            target.setOutlineColor(sf::Color::Red);
-            randX = rand() % 540 + 50;
-            randY = rand() % 360 + 50;
-            target.setPosition(randX, randY);
-            dt = sf::seconds(0);
-        }
-        dt += deltaClock.restart();
+		
+		if (playPressed)
+		{
+			std::call_once(onceFlag, [&] {deltaClock.restart();});
+			
+			aim.setPosition(webcamThread.getX(), webcamThread.getY());
+			if (dt <= sf::seconds(SPAWN_DELAY))
+			{
+				window.clear();
+
+				pointTotal.setString("Points: " + to_string(points));
+				window.draw(pointTotal);
+				window.draw(target);
+				if (webcamThread.getX() < 0 || webcamThread.getY() < 0) {
+					window.draw(gunpointNotFound);
+				}
+				else {
+					window.draw(aim);
+				}
+			}
+			else
+			{
+				targetShot = false;
+				target.setOutlineColor(sf::Color::Red);
+				randX = rand() % 540 + 50;
+				randY = rand() % 360 + 50;
+				target.setPosition(randX, randY);
+				dt = sf::seconds(0);
+			}
+			dt += deltaClock.restart();
+		}
+		else
+		{
+			window.clear();
+			menu.draw(window);
+		}
         window.display();
     }
     return 0;
