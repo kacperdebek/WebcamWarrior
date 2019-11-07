2e3fdbee30327675b38b9bff4f702e0a7835fbc4
pk-37 <pk-37@sr.dmcs.pl>
2019-11-07 20:03:40 +0100

    Wersja testowa uodpornienia sterowania na zakłócenia

--- WebcamControl.cpp
+++ WebcamControl.cpp
@@ -5,6 +5,10 @@ void WebcamControl::run()
     Mat image;
     Mat image_gray;
     Mat result;
+	Mat res;
+
+	vector<vector<Point> >contours;
+	vector<Vec4i>hierarchy;
     if (!vcap.open(0))
     {
         cout << "Error opening video stream or file" << endl;
@@ -20,14 +24,38 @@ void WebcamControl::run()
 		resize(image, image, Size(1280, 720));
         flip(image, image, 1);
         cvtColor(image, image_gray, COLOR_BGR2GRAY);
-        threshold(image_gray, result, webcamThreshold, 255, THRESH_BINARY);
-        Moments m = moments(result, true);
+        //threshold(image_gray, result, webcamThreshold, 255, THRESH_BINARY);
+		threshold(image_gray, res, webcamThreshold, 255, THRESH_BINARY);
+        /*Moments m = moments(result, true);
         x = m.m10 / m.m00;
         y = m.m01 / m.m00;
         Point p(x, y);
         circle(result, p, 5, Scalar(128, 0, 0), -1);
+		*/
+		int savedContour = -1;
+		double maxArea = 0.0;
+		findContours(res, contours, hierarchy, RETR_EXTERNAL,CHAIN_APPROX_SIMPLE, Point());
+		for (int i = 0; i < contours.size(); i++)
+		{
+			double area = contourArea(contours[i]);
+			if (area > maxArea)
+			{
+				maxArea = area;
+				savedContour = i;
+			}
+			
+		}
+		Mat img(720, 1280, CV_8UC3, Scalar(0, 0, 0));
+		cvtColor(img, img, COLOR_BGR2GRAY);
+		drawContours(img, contours, savedContour, Scalar(255, 255, 255), FILLED, 8);
+		Moments m2 = moments(img, true);
+		x = m2.m10 / m2.m00;
+		y = m2.m01 / m2.m00;
+		Point p2(x, y);
+		circle(img, p2, 5, Scalar(128, 0, 0), -1);
 		
-        imshow("Cam", result);
+        //imshow("Cam", result);
+		imshow("Camv2", img);
         if (waitKey(1) == 27)
             break;
     }
