abd2742d17b42aa389abef73556ef8b1556e9227
pk-37 <pk-37@sr.dmcs.pl>
2020-01-12 14:46:41 +0100

    Rozdzielenie WebcamControl na mniejsze funkcje

--- WebcamControl.cpp
+++ WebcamControl.cpp
@@ -1,72 +1,70 @@
 #include "WebcamControl.hpp"
 
-void WebcamControl::run()
-{
+void WebcamControl::getBiggestContour(cv::Mat& res, std::vector<std::vector<cv::Point>>& contours, std::vector<cv::Vec4i>& hierarchy, int& savedContour) {
+	double maxArea = 0.0;
+	findContours(res, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point());
+	for (int i = 0; i < contours.size(); i++){
+		double area = contourArea(contours[i]);
+		if (area > maxArea){
+			maxArea = area;
+			savedContour = i;
+		}
+	}
+	if (maxArea < 6000)
+		savedContour = -1;
+}
+void WebcamControl::drawBiggestBlob(cv::Mat& img, std::vector<std::vector<cv::Point>>& contours, int savedContour) {
+	cvtColor(img, img, COLOR_BGR2GRAY);
+	drawContours(img, contours, savedContour, Scalar(255, 255, 255), FILLED, 8);
+}
+void WebcamControl::getAndDrawCentroid(cv::Mat& img) {
+	Moments m2 = moments(img, true);
+	x = m2.m10 / m2.m00;
+	y = m2.m01 / m2.m00;
+	Point p2(x, y);
+	circle(img, p2, 5, Scalar(128, 0, 0), -1);
+}
+void WebcamControl::prepareAndThresholdFrame(cv::Mat& image) {
+	resize(image, image, Size(1280, 720));
+	flip(image, image, 1);
+	cvtColor(image, image, COLOR_BGR2GRAY);
+	threshold(image, image, this->webcamThreshold, 255, THRESH_BINARY);
+}
+void WebcamControl::run() {
 	VideoCapture vcap;
 	Mat image;
-	Mat image_gray;
-	Mat result;
 	Mat res;
-
 	vector<vector<Point> >contours;
 	vector<Vec4i>hierarchy;
-	if (!vcap.open(0))
-	{
+	if (!vcap.open(0)) {
 		cout << "Error opening video stream or file" << endl;
 		return;
 	}
-	while (true)
-	{
-		if (!vcap.read(image))
-		{
+	while (true) {
+		if (!vcap.read(image)){
 			cout << "No frame" << endl;
 			waitKey();
 		}
-		resize(image, image, Size(1280, 720));
-		flip(image, image, 1);
-		cvtColor(image, image_gray, COLOR_BGR2GRAY);
-		threshold(image_gray, res, webcamThreshold, 255, THRESH_BINARY);
+		prepareAndThresholdFrame(image);
 		int savedContour = -1;
-		double maxArea = 0.0;
-		findContours(res, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point());
-		for (int i = 0; i < contours.size(); i++)
-		{
-			double area = contourArea(contours[i]);
-			if (area > maxArea)
-			{
-				maxArea = area;
-				savedContour = i;
-			}
-		}
-		if(maxArea < 6000)
-			savedContour = -1;
+		getBiggestContour(res, contours, hierarchy, savedContour);
 		Mat img(720, 1280, CV_8UC3, Scalar(0, 0, 0));
-		cvtColor(img, img, COLOR_BGR2GRAY);
-		drawContours(img, contours, savedContour, Scalar(255, 255, 255), FILLED, 8);
-		Moments m2 = moments(img, true);
-		x = m2.m10 / m2.m00;
-		y = m2.m01 / m2.m00;
-		Point p2(x, y);
-		circle(img, p2, 5, Scalar(128, 0, 0), -1);
-
+		drawBiggestBlob(img, contours, savedContour);
+		getAndDrawCentroid(img);
 		imshow("Cam", img);
 		if (waitKey(1) == 27)
 			break;
 	}
 }
-int WebcamControl::getX()
-{
+int WebcamControl::getX() {
 	return this->x;
 }
-int WebcamControl::getY()
-{
+int WebcamControl::getY() {
 	return this->y;
 }
-void WebcamControl::setThreshold(int targetThreshold)
-{
+void WebcamControl::setThreshold(int targetThreshold) {
 	this->webcamThreshold = targetThreshold;
 }
-int WebcamControl::getThreshold()
-{
+int WebcamControl::getThreshold() {
 	return this->webcamThreshold;
 }
--- WebcamControl.hpp
+++ WebcamControl.hpp
@@ -9,7 +9,11 @@ private:
 	int x;
 	int y;
 	int webcamThreshold;
+	void getBiggestContour(cv::Mat& res, std::vector<std::vector<cv::Point>>& contours, std::vector<cv::Vec4i>& hierarchy, int& savedContour);
+	void drawBiggestBlob(cv::Mat& img, std::vector<std::vector<cv::Point>>& contours, int savedContour);
+	void getAndDrawCentroid(cv::Mat& img);
 public:
+	void prepareAndThresholdFrame(cv::Mat& image);
 	void run();
 	int getX();
 	int getY();
